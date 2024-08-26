6dd758ae83be6da9ca3109a6247c1e17082b9334
Tareq675 <thtalukder@gmail.com>
2024-08-27 02:20:19 +0300

    complete heder section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+          kkthy: 'rgb(139, 255, 74)',
+          
+        }
+      },
+    },
+    plugins: [],
+  }
